82ec817778f0893120b86e114a6bfa0dd799cae4
gabriella-silva <gabriellaferreira534@gmail.com>
2025-05-29 19:27:52 -0300

    .

--- app/Http/Controllers/InstituicoesController.php
+++ app/Http/Controllers/InstituicoesController.php
@@ -4,8 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Instituicoes;
 use Illuminate\Http\Request;
-use Illuminate\Http\RedirectResponse;
-use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Validator;
 
 
--- routes/api.php
+++ routes/api.php
@@ -5,44 +5,34 @@ use App\Http\Controllers\FormularioController;
 use App\Http\Controllers\InstituicoesController;
 use App\Http\Controllers\UsuariosController;
 
-//quando alguém acessar GET /user, o método index do InstituicoesController será chamado.
+// Rota pública para listar usuários (exemplo)
+// Se quiser manter protegida, pode usar 'auth' (session) no lugar de 'auth:api'
+// Route::middleware('auth')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
+// Rotas Admin sem autenticação JWT (abertas)
+Route::post('/admin',[AdminController::class, 'store']);
+Route::put('/admin/{id}',[AdminController::class,'update']);
+Route::delete('/admin/{id}',[AdminController::class,'destroy']);
+Route::get('/admin',[AdminController::class,'index']);
+Route::get('/admin/{id}',[AdminController::class,'show']);
 
-// Rota protegida que retorna o usuário autenticado (exemplo)
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Rotas Formulario abertas
+Route::post('/formularios', [FormularioController::class, 'store']);
+Route::patch('/formularios/{formulario}/status', [FormularioController::class, 'atualizarStatus'])->name('formularios.status');
+Route::get('/instituicoes/{id}/formularios', [FormularioController::class, 'listarPorInstituicao'])->name('instituicoes.formularios');
 
-// Rotas Admin protegidas por JWT
-Route::middleware('auth:api')->group(function () {
-    Route::post('/admin',[AdminController::class, 'store']);
-    Route::put('/admin/{id}',[AdminController::class,'update']);
-    Route::delete('/admin/{id}',[AdminController::class,'destroy']);
-    Route::get('/admin',[AdminController::class,'index']);
-    Route::get('/admin/{id}',[AdminController::class,'show']);
-});
+// Rotas Instituicoes abertas
+Route::post('/instituicoes',[InstituicoesController::class, 'store']);
+Route::put('/instituicoes/{id}',[InstituicoesController::class, 'update']);
+Route::delete('/instituicoes/{id}',[InstituicoesController::class, 'destroy']);
+Route::get('/instituicoes',[InstituicoesController::class, 'index']);
+Route::get('/instituicoes/{id}',[InstituicoesController::class, 'show']);
 
-// Rotas Formulario protegidas (exemplo)
-Route::middleware('auth:api')->group(function () {
-    Route::post('/formularios', [FormularioController::class, 'store']);
-    Route::patch('/formularios/{formulario}/status', [FormularioController::class, 'atualizarStatus'])->name('formularios.status');
-    Route::get('/instituicoes/{id}/formularios', [FormularioController::class, 'listarPorInstituicao'])->name('instituicoes.formularios');
-});
-
-// Rotas Instituicoes protegidas
-Route::middleware('auth:api')->group(function () {
-    Route::post('/instituicoes',[InstituicoesController::class, 'store']);
-    Route::put('/instituicoes/{id}',[InstituicoesController::class, 'update']);
-    Route::delete('/instituicoes/{id}',[InstituicoesController::class, 'destroy']);
-    Route::get('/instituicoes',[InstituicoesController::class, 'index']);
-    Route::get('/instituicoes/{id}',[InstituicoesController::class, 'show']);
-});
-
-// Rotas Usuarios protegidas
-Route::middleware('auth:api')->group(function () {
-    Route::post('/usuarios',[UsuariosController::class, 'store']);
-    Route::put('/usuarios/{id}',[UsuariosController::class, 'update']);
-    Route::delete('/usuarios/{id}',[UsuariosController::class, 'destroy']);
-    Route::get('/usuarios',[UsuariosController::class, 'index']);
-    Route::get('/usuarios/{id}',[UsuariosController::class, 'show']);
-});
+// Rotas Usuarios abertas
+Route::post('/usuarios',[UsuariosController::class, 'store']);
+Route::put('/usuarios/{id}',[UsuariosController::class, 'update']);
+Route::delete('/usuarios/{id}',[UsuariosController::class, 'destroy']);
+Route::get('/usuarios',[UsuariosController::class, 'index']);
+Route::get('/usuarios/{id}',[UsuariosController::class, 'show']);
