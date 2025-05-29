3ba42f4fddb8bac9cfe18eb66a7258a93a5002aa
gabriella-silva <gabriellaferreira534@gmail.com>
2025-05-29 17:55:49 -0300

    .

--- resources/js/Pages/Ong/LoginIniciativa.jsx
+++ resources/js/Pages/Ong/LoginIniciativa.jsx
@@ -43,7 +43,7 @@ const handleLogin = async () => {
   const response = await fetch('http://localhost:8000/api/login', {
     method: 'POST',
     headers: { 'Content-Type': 'application/json' },
-body: JSON.stringify({ email, password: senha }),
+body: JSON.stringify({ email, senha }),
   });
 
   const data = await response.json();
